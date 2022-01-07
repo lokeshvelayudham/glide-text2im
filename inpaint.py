3ec76fa468ce28c0d18721bd13f982ca93da5a30
Nerdy Rodent <74688049+nerdyrodent@users.noreply.github.com>
2022-01-07 00:09:21 +0000

    Add files via upload

--- inpaint.py
+++ inpaint.py
@@ -1,6 +1,10 @@
+from typing import Tuple
 from PIL import Image
-import torch as th
 from datetime import datetime
+
+import numpy as np
+import torch as th
+import torch.nn.functional as F
 
 from glide_text2im.download import load_checkpoint
 from glide_text2im.model_creation import (
@@ -9,21 +13,38 @@ from glide_text2im.model_creation import (
     model_and_diffusion_defaults_upsampler
 )
 
+def read_image(path: str, size: int = 256) -> Tuple[th.Tensor, th.Tensor]:
+    pil_img = Image.open(path).convert('RGB')
+    pil_img = pil_img.resize((size, size), resample=Image.BICUBIC)
+    img = np.array(pil_img)
+    return th.from_numpy(img)[None].permute(0, 3, 1, 2).float() / 127.5 - 1
+
+#########################################
 # Sampling parameters
-prompt = "A nerdy rodent"
+prompt = "a nerdy rodent in a field"
 batch_size = 4
-guidance_scale = 3.0
+guidance_scale = 5.0
 
 # Tune this parameter to control the sharpness of 256x256 images.
 # A value of 1.0 is sharper, but sometimes results in grainy artifacts.
 upsample_temp = 0.997
 
+# Source image we are inpainting
+source_image_256 = read_image('notebooks/grass.png', size=256)
+source_image_64 = read_image('notebooks/grass.png', size=64)
 
-###############
+# The mask should always be a boolean 64x64 mask, and then we
+# can upsample it for the second stage.
+source_mask_64 = th.ones_like(source_image_64)[:, :1]
+source_mask_64[:, :, 20:] = 0
+source_mask_256 = F.interpolate(source_mask_64, (256, 256), mode='nearest')
+#########################################
+
+
+#########################################
 # This notebook supports both CPU and GPU.
 # On CPU, generating one sample may take on the order of 20 minutes.
 # On a GPU, it should be under a minute.
-
 has_cuda = th.cuda.is_available()
 device = th.device('cpu' if not has_cuda else 'cuda')
 
@@ -32,6 +53,7 @@ xprompt = prompt.replace(" ", "_")[:] + "-gs_" + str(guidance_scale)
 
 # Create base model.
 options = model_and_diffusion_defaults()
+options['inpaint'] = True
 options['use_fp16'] = has_cuda
 options['timestep_respacing'] = '100' # use 100 diffusion steps for fast sampling
 model, diffusion = create_model_and_diffusion(**options)
@@ -39,11 +61,12 @@ model.eval()
 if has_cuda:
     model.convert_to_fp16()
 model.to(device)
-model.load_state_dict(load_checkpoint('base', device))
+model.load_state_dict(load_checkpoint('base-inpaint', device))
 print('total base parameters', sum(x.numel() for x in model.parameters()))
 
 # Create upsampler model.
 options_up = model_and_diffusion_defaults_upsampler()
+options_up['inpaint'] = True
 options_up['use_fp16'] = has_cuda
 options_up['timestep_respacing'] = 'fast27' # use 27 diffusion steps for very fast sampling
 model_up, diffusion_up = create_model_and_diffusion(**options_up)
@@ -51,9 +74,8 @@ model_up.eval()
 if has_cuda:
     model_up.convert_to_fp16()
 model_up.to(device)
-model_up.load_state_dict(load_checkpoint('upsample', device))
+model_up.load_state_dict(load_checkpoint('upsample-inpaint', device))
 print('total upsampler parameters', sum(x.numel() for x in model_up.parameters()))
-
 
 def save_images(batch: th.Tensor):
     """ Save images """
@@ -69,7 +91,10 @@ def save_images(batch: th.Tensor):
         test_single = scaled.select(0,_)
         test_reshape = test_single.permute(1, 2, 0).reshape([batch.shape[2], -1, 3])
         Image.fromarray(test_reshape.numpy()).save(f'{xprompt}-{_}-{stamp}.png')
-    
+
+# Visualise the image we are inpainting - if you want to, uncomment
+#save_images(source_image_256 * source_mask_256)
+
 
 ##############################
 # Sample from the base model #
@@ -97,9 +122,13 @@ model_kwargs = dict(
         dtype=th.bool,
         device=device,
     ),
+
+    # Masked inpainting image
+    inpaint_image=(source_image_64 * source_mask_64).repeat(full_batch_size, 1, 1, 1).to(device),
+    inpaint_mask=source_mask_64.repeat(full_batch_size, 1, 1, 1).to(device),
 )
 
-# Create a classifier-free guidance sampling function
+# Create an classifier-free guidance sampling function
 def model_fn(x_t, ts, **kwargs):
     half = x_t[: len(x_t) // 2]
     combined = th.cat([half, half], dim=0)
@@ -109,6 +138,14 @@ def model_fn(x_t, ts, **kwargs):
     half_eps = uncond_eps + guidance_scale * (cond_eps - uncond_eps)
     eps = th.cat([half_eps, half_eps], dim=0)
     return th.cat([eps, rest], dim=1)
+
+def denoised_fn(x_start):
+    # Force the model to have the exact right x_start predictions
+    # for the part of the image which is known.
+    return (
+        x_start * (1 - model_kwargs['inpaint_mask'])
+        + model_kwargs['inpaint_image'] * model_kwargs['inpaint_mask']
+    )
 
 # Sample from the base model.
 model.del_cache()
@@ -120,11 +157,12 @@ samples = diffusion.p_sample_loop(
     progress=True,
     model_kwargs=model_kwargs,
     cond_fn=None,
+    denoised_fn=denoised_fn,
 )[:batch_size]
 model.del_cache()
 
-# 64x64 samples = way too small to bother saving ;)
-#save_images(samples)
+# 64x64 output - not worth saving, but uncomment if you like!
+# save_images(samples)
 
 ##############################
 # Upsample the 64x64 samples #
@@ -149,12 +187,24 @@ model_kwargs = dict(
         dtype=th.bool,
         device=device,
     ),
+
+    # Masked inpainting image.
+    inpaint_image=(source_image_256 * source_mask_256).repeat(batch_size, 1, 1, 1).to(device),
+    inpaint_mask=source_mask_256.repeat(batch_size, 1, 1, 1).to(device),
 )
+
+def denoised_fn(x_start):
+    # Force the model to have the exact right x_start predictions
+    # for the part of the image which is known.
+    return (
+        x_start * (1 - model_kwargs['inpaint_mask'])
+        + model_kwargs['inpaint_image'] * model_kwargs['inpaint_mask']
+    )
 
 # Sample from the base model.
 model_up.del_cache()
 up_shape = (batch_size, 3, options_up["image_size"], options_up["image_size"])
-up_samples = diffusion_up.ddim_sample_loop(
+up_samples = diffusion_up.p_sample_loop(
     model_up,
     up_shape,
     noise=th.randn(up_shape, device=device) * upsample_temp,
@@ -163,8 +213,9 @@ up_samples = diffusion_up.ddim_sample_loop(
     progress=True,
     model_kwargs=model_kwargs,
     cond_fn=None,
+    denoised_fn=denoised_fn,
 )[:batch_size]
 model_up.del_cache()
 
-# Save the output
+# Show the output
 save_images(up_samples)
